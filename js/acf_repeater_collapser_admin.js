187de7955161a55b45ce2573b334a7cb43ac7b1a
Andy Richardson <andy@andimal.co>
2013-11-06 15:12:18 -0600

    Use acf stock toggle for flexible content fields, fields collapsed by default

--- js/acf_repeater_collapser_admin.js
+++ js/acf_repeater_collapser_admin.js
@@ -2,16 +2,38 @@ jQuery(document).ready(function($) {
 
 	// toggle the class that collapses the repeater
 	// toggle appropriate button text
-	function acf_repeater_toggle( event ) {
-	    // Toggle Fields
-	    $(this).parent().toggleClass('collapsed-repeater');
-	    
-	    // Nice Button Text
-	    if( $(this).attr('value') == 'Collapse Fields' ) {
-	    	$(this).attr('value', 'Expand Fields');
-	    } else {
-	    	$(this).attr('value', 'Collapse Fields');
-	    }
+	function acf_repeater_toggle( obj ) {
+    var $button = $(this);
+    
+    // If the function is called by something other than a click event (on load, for instance),
+    // set $button to the object that is supplied as a parameter
+    if( !$button.hasClass('field-repeater-toggle') ) {
+    	$button = obj;
+    }
+
+    // Toggle Fields
+    // $(this).parent().toggleClass('collapsed-repeater');
+
+    if( $button.attr('value') == 'Collapse Fields' ) {
+    	$button.attr('value', 'Expand Fields');
+
+			// Toggle all open fields to closed
+			$('.acf-flexible-content .values .layout').each(function() {
+				if( $(this).attr('data-toggle') === 'open' || !$(this).attr('data-toggle') ) {
+					acf.fields.flexible_content.toggle( $(this) );
+				}
+			});
+
+    } else {
+    	$button.attr('value', 'Collapse Fields');
+
+    	// Toggle all closed fields to open
+			$('.acf-flexible-content .values .layout').each(function() {
+				if( $(this).attr('data-toggle') === 'closed' ) {
+					acf.fields.flexible_content.toggle( $(this) );
+				}
+			});
+    }
 	}
 
 	// HTML to put above each repeater instance
@@ -22,6 +44,11 @@ jQuery(document).ready(function($) {
 		if( $( '.acf-input-table', $(this) ).hasClass('row_layout') ) {
 			$(this).prepend( $collapseButton );
 		}
+	});
+
+	// Collapse flexible content rows on load
+	$('.field_type-flexible_content .field-repeater-toggle').each(function() {
+		acf_repeater_toggle( $(this) );
 	});
 
 	// bind the click event to the toggle function
